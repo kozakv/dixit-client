6ceb540858ecd3fb3fe3856fbb575e0b3c08a80b
dima levchenko <ldimat@gmail.com>
2015-03-14 14:26:45 +0200

    add client deploy fabric script

---
+++ fabfile.py
@@ -0,0 +1,8 @@
+from __future__ import with_statement
+from fabric.api import *
+
+# env.hosts = ['dima@192.168.0.16']
+
+def deploy_client(glassfish_home, domain_name = "domain1"):
+    print(glassfish_home + "/glassfish/domains/" + domain_name + "/applications/dixit-0.0.1/")
+    # put("dist/*", glassfish_home + "/glassfish/domains/" + domain_name + "/applications/dixit-0.0.1/")
