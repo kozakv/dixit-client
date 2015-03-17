5ce46dd83c1182e4f14d17c5e8b71077dd3c3992
dima levchenko <ldimat@gmail.com>
2015-03-17 22:11:02 +0200

    enhance fabric file

--- fabfile.py
+++ fabfile.py
@@ -4,5 +4,5 @@ from fabric.api import *
 # env.hosts = ['dima@192.168.0.16']
 
 def deploy_client(glassfish_home, domain_name = "domain1"):
-    print(glassfish_home + "/glassfish/domains/" + domain_name + "/applications/dixit-0.0.1/")
-    # put("dist/*", glassfish_home + "/glassfish/domains/" + domain_name + "/applications/dixit-0.0.1/")
+    # print(glassfish_home + "/glassfish/domains/" + domain_name + "/applications/dixit-0.0.1/")
+    put("dist/*", glassfish_home + "/glassfish/domains/" + domain_name + "/applications/dixit-0.0.1/")
