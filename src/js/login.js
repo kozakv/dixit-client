e3d8a92590654b7d155b4b9927ef97da0505db6a
unknownv <unknownv@gmail.com>
2015-03-22 22:42:43 +0200

    Login test

--- src/js/login.js
+++ src/js/login.js
@@ -1,31 +1,38 @@
 DixitViews.Login = View.extend({
   template: JST.login,
 
-  afterRender: function() {
+  events: {
+    "click #login-button": "onLoginClick",
+    "click #logout-button": "onLogoutClick",
+    "click #gotorooms-button": "navigateToRooms"
+  },
 
-	$("#login-button", this.$el).click(this.onLoginClick.bind(this));
+  afterRender: function() {
+	
+  /*$("#login-button", this.$el).click(this.onLoginClick.bind(this));
 
 	$("#logout-button", this.$el).click(this.onLogoutClick.bind(this));
 
-	$("#gotorooms-button", this.$el).click(function() {DixitRouter.navigateTo("Rooms"); });	
+	$("#gotorooms-button", this.$el).click(this.navigateToRooms.bind(this));*/	
 
 
 	DixitServer.loginInfo(
-    function(loginId, nickname) {
-  		if (nickname != null) {
-				$(".login-form, .waiting_spinner, .server_failed", this.$el).hide();
-				$(".name-form", this.$el).show();
-				$("#name", this.$el).html("Hello,<br/><b>" + nickname + "</b>!");		  				
-  		}
-  		else {
-  			$(".login-form", this.$el).show();
-				$(".name-form, .waiting_spinner, .server_failed", this.$el).hide();
-  		};
-    },
+    this.ifLogged.bind(this),
     this.ifServerFailed.bind(this)
   );
 },
 
+
+ifLogged: function(loginId, nickname) {
+  if (nickname != null) {
+    $(".login-form, .waiting_spinner, .server_failed", this.$el).hide();
+    $(".name-form", this.$el).show();
+    $("#name", this.$el).html("Hello,<br/><b>" + nickname + "</b>!");             
+   } else {
+    $(".login-form", this.$el).show();
+    $(".name-form, .waiting_spinner, .server_failed", this.$el).hide();
+  };
+},
 
 onLoginClick: function() {
   $(".waiting_spinner", this.$el).show();
@@ -61,6 +68,10 @@ ifLogoutSuccess: function() {
   $(".waiting_spinner", this.$el).hide();
   $(".login-form", this.$el).show();
   console.log("Log out successful");
+},
+
+navigateToRooms: function() {
+  DixitRouter.navigateTo("Rooms");
 }
 
 });
